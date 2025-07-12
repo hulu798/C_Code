24fb279ed82a8ca2c9a1f1261eb655c7d00ec2dc
Dawn <3049366180@qq.com>
2025-07-12 17:29:59 +0800

    fix 25-5\3\stringFunction.c my_strcmp

--- 25-5/3/stringFunction.c
+++ 25-5/3/stringFunction.c
@@ -38,7 +38,7 @@ int my_strcmp(const char *str1,const char *str2)
 {
     assert(str1 && str2);
     while(*str1++==*str2++)
-        if(!*--str1) return 0;
+        if(!*(str1-1)) return 0;
     return *str1-*str2;
 }
 
